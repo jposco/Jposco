a457baa7df84186241b64e26e7afa24a15034833
jposco <godeok24@naver.com>
2023-03-24 00:10:30 +0900

    last

---
+++ day2-VS/day2-review/day2-review/day2-review.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+//#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    string greeting;
+
+    cout << "이름입력" << endl;
+    cin >> name;
+
+    greeting = "안녕하세요" + name + "님";
+
+    cout << greeting << endl;
+
+    cout << name << "은 총 " << name.length() << "글자 입니다." << endl;
+
+    cout << name << "의 첫 글자는 " << name[0] << "입니다." << endl;
+ 
+}
